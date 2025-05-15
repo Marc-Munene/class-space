b68648d77a7e059a933514313b85cca8365bfa47
Marc Munene <marcmunene104@gmail.com>
2025-05-15 23:08:31 +0300

    home route controller

--- Server/src/server.js
+++ Server/src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import "dotenv/config";
 
 import { connectDB } from "./database/config.js";
+import { getHome } from "./controllers/home.js";
 
 const app = express();
 
@@ -10,6 +11,8 @@ const app = express();
 connectDB();
 
 const PORT = process.env.PORT;
+
+app.get("/", getHome);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
