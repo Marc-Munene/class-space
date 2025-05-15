ea08d921624fe6d274a8287363eeb3e186290f6b
Marc Munene <marcmunene104@gmail.com>
2025-05-15 21:29:14 +0300

    user model

---
+++ Server/src/database/models/User.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["rep, student, admin, user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = new model("user", userSchema);
+
+export { User };
