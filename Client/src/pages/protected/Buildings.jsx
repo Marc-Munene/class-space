754b6ba1938626573669377982640dd573589a4d
Marc Munene <marcmunene104@gmail.com>
2025-05-28 14:26:09 +0300

    buildings page

--- Client/src/pages/protected/Buildings.jsx
+++ Client/src/pages/protected/Buildings.jsx
@@ -28,7 +28,25 @@ const Buildings = () => {
               <td className="py-2 sm:py-3 text-center">1</td>
               <td className="py-2 sm:py-3 text-center">SPA</td>
               <td className="py-2 sm:py-3 text-center">20</td>
-              <td className="py-2 sm:py-3 text-center">View Details</td>
+              <td className="py-2 sm:py-3 text-center text-DeepBlue">
+                View Details
+              </td>
+            </tr>
+            <tr className="border-b border-gray-300">
+              <td className="py-2 sm:py-3 text-center">2</td>
+              <td className="py-2 sm:py-3 text-center">CLB</td>
+              <td className="py-2 sm:py-3 text-center">24</td>
+              <td className="py-2 sm:py-3 text-center text-DeepBlue">
+                View Details
+              </td>
+            </tr>
+            <tr className="border-b border-gray-300">
+              <td className="py-2 sm:py-3 text-center">3</td>
+              <td className="py-2 sm:py-3 text-center">NCLB</td>
+              <td className="py-2 sm:py-3 text-center">19</td>
+              <td className="py-2 sm:py-3 text-center text-DeepBlue">
+                View Details
+              </td>
             </tr>
           </tbody>
         </table>
