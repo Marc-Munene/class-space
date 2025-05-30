55e4667b2cf6ff3f9c04bbe88bca66df3a7111c6
Marc Munene <marcmunene104@gmail.com>
2025-05-30 12:55:53 +0300

    buildings

--- Client/src/pages/protected/Buildings.jsx
+++ Client/src/pages/protected/Buildings.jsx
@@ -14,7 +14,7 @@ const Buildings = () => {
       </div>
 
       <div className="overflow-x-auto rounded-lg shadow-sm mt-5">
-        <table className="w-full text-left text-sm">
+        <table className="w-full text-left text-base">
           <thead className="bg-gray-200">
             <tr>
               <th className="p-2 sm:p-3 text-center">#</th>
@@ -24,30 +24,21 @@ const Buildings = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="border-b border-gray-300">
-              <td className="py-2 sm:py-3 text-center">1</td>
-              <td className="py-2 sm:py-3 text-center">SPA</td>
-              <td className="py-2 sm:py-3 text-center">20</td>
-              <td className="py-2 sm:py-3 text-center text-DeepBlue">
-                View Details
-              </td>
-            </tr>
-            <tr className="border-b border-gray-300">
-              <td className="py-2 sm:py-3 text-center">2</td>
-              <td className="py-2 sm:py-3 text-center">CLB</td>
-              <td className="py-2 sm:py-3 text-center">24</td>
-              <td className="py-2 sm:py-3 text-center text-DeepBlue">
-                View Details
-              </td>
-            </tr>
-            <tr className="border-b border-gray-300">
-              <td className="py-2 sm:py-3 text-center">3</td>
-              <td className="py-2 sm:py-3 text-center">NCLB</td>
-              <td className="py-2 sm:py-3 text-center">19</td>
-              <td className="py-2 sm:py-3 text-center text-DeepBlue">
-                View Details
-              </td>
-            </tr>
+            {buildings?.length > 0 &&
+              buildings.map((building, i) => (
+                <tr key={i} className="border-b border-gray-300">
+                  <td className="py-2 sm:py-3 text-center">{i + 1}</td>
+                  <td className="py-2 sm:py-3 text-center">
+                    {building.buildingName}
+                  </td>
+                  <td className="py-2 sm:py-3 text-center">
+                    {building.rooms.length}
+                  </td>
+                  <td className="py-2 sm:py-3 text-center text-DeepBlue">
+                    View Details
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
