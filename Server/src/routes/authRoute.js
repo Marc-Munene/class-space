43a7854951894eb720d6df295dd43542b2c2d8df
Marc Munene <marcmunene104@gmail.com>
2025-06-06 12:35:02 +0300

    google login authentication route

--- Server/src/routes/authRoute.js
+++ Server/src/routes/authRoute.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { currentUser, login, logout, signUp } from "../controllers/auth.js";
+import {
+  currentUser,
+  googleAuth,
+  login,
+  logout,
+  signUp,
+} from "../controllers/auth.js";
 import { userAuthentication } from "../middleware/AuthMiddleware.js";
 
 const authRouter = Router();
@@ -7,6 +13,8 @@ const authRouter = Router();
 authRouter.post("/auth/signup", signUp);
 
 authRouter.post("/auth/login", login);
+
+authRouter.post("/auth/google", googleAuth);
 
 authRouter.get("/auth/me", userAuthentication, currentUser);
 
